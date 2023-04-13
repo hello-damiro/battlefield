54967b00988e0086be3ff61dd6e09e4db6d1e1ed
damiro <hello.damiro@gmail.com>
2023-04-14 01:18:13 +0800

    cell disable on click

--- src/index.js
+++ src/index.js
@@ -8,3 +8,11 @@ const grid = 10;
 
 createMap(map, grid);
 createMap(miniMap, grid, true).disableCells();
+
+const cells = _$('.cell');
+cells.forEach((cell) => {
+    cell.addEventListener('click', () => {
+        cell.classList.add('hit');
+        cell.classList.add('no-click');
+    });
+});
