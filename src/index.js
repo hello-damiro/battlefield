7b381e6ac7fbcac572bffb8404f644e69f76a3af
damiro <hello.damiro@gmail.com>
2023-04-14 00:54:23 +0800

    create map module

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './styles/style.css';
+import { $, _$ } from './js/helpers';
+import { createMap } from './js/create-map';
+
+const map = $('.main-map');
+const miniMap = $('.monitor-map');
+const grid = 10;
+
+createMap(map, grid);
+createMap(miniMap, grid, true).disableCells();
---
+++ src/js/create-map.js
@@ -0,0 +1,32 @@
+export const createMap = (div, length, monitor) => {
+    createColumn(div);
+    function createColumn(parent) {
+        const column = document.createElement('div');
+        column.classList.add('column');
+        parent.appendChild(column);
+        for (let index = 0; index < length; index++) createRowCells(column);
+        monitor ? column.classList.add('border-blue') : column.classList.add('border-red');
+    }
+
+    function createRowCells(parent) {
+        const rowCells = document.createElement('div');
+        rowCells.classList.add('row');
+        parent.appendChild(rowCells);
+        for (let index = 0; index < length; index++) createCell(rowCells);
+    }
+
+    function createCell(parent) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        parent.appendChild(cell);
+    }
+
+    function disableCells() {
+        const cells = div.querySelectorAll('.cell');
+        cells.forEach((cell) => cell.classList.add('no-click'));
+    }
+
+    return {
+        disableCells: disableCells,
+    };
+};
