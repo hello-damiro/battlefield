7d7f862c46461f4537db96a16a77f187152d8c44
damiro <hello.damiro@gmail.com>
2023-04-16 02:40:58 +0800

    overlap is detected. need to work on highlighting

--- src/js/ui/setup-game.js
+++ src/js/ui/setup-game.js
@@ -10,6 +10,7 @@ export const SetupGame = (map, ships) => {
     let selectedShip = { x: 0, y: 0 };
     let dockedShips = [];
     let occupiedCells = [];
+    let occupiedByShip = [];
 
     const startButton = $('.start');
     startButton.addEventListener('click', () => {
@@ -80,90 +81,136 @@ export const SetupGame = (map, ships) => {
         const cells = map.querySelectorAll('.cell');
         cells.forEach((cell, index) => {
             let isPermanent = false;
-            const nucleus = cell.querySelector('.nucleus');
             const x = cell.getAttribute('data-x');
             const y = cell.getAttribute('data-y');
-            cell.addEventListener('click', () => {
+            const nucleus = cell.querySelector('.nucleus');
+
+            const handleCellClick = () => {
                 isPermanent = true;
-                const type = dockedShips[shipIndex].type;
-                const length = dockedShips[shipIndex].length;
-                if (isAllowed(x, y, length, isVertical)) {
-                    hoverNucleusHandler(nucleus, type, true, isVertical, isPermanent);
-                    clickCellHandler(cell);
-                    reserveCells(x, y, length, isVertical);
+                const { type, length } = dockedShips[shipIndex];
+                if (hasOverlap(getShipCells(x, y, length, isVertical))) {
+                    highlightInvalid(x, y, isVertical, true);
+                } else {
+                    if (isAllowed(x, y, length, isVertical)) {
+                        setShipInPlace(cell);
+                        reserveCells(x, y, length, isVertical);
+                        hoverNucleus(nucleus, type, true, isVertical, isPermanent);
+                    }
                 }
-            });
-            cell.addEventListener('mouseover', () => {
-                const type = dockedShips[shipIndex].type;
-                const length = dockedShips[shipIndex].length;
-                if (isAllowed(x, y, length, isVertical))
-                    hoverNucleusHandler(nucleus, type, true, isVertical, isPermanent);
-                else highlightInvalid(x, y, isVertical, true);
-            });
-            cell.addEventListener('mouseout', () => {
-                const type = dockedShips[shipIndex].type;
-                const length = dockedShips[shipIndex].length;
-                if (isAllowed(x, y, length, isVertical))
-                    hoverNucleusHandler(nucleus, type, false, isVertical, isPermanent);
-                else highlightInvalid(x, y, isVertical, false);
-            });
+            };
+
+            const handleCellHover = (isOver) => {
+                const { type, length } = dockedShips[shipIndex];
+                if (hasOverlap(getShipCells(x, y, length, isVertical))) {
+                    highlightInvalid(x, y, isVertical, isOver);
+                } else {
+                    if (isAllowed(x, y, length, isVertical))
+                        hoverNucleus(nucleus, type, isOver, isVertical, isPermanent);
+                    else highlightInvalid(x, y, isVertical, isOver);
+                }
+            };
+
+            cell.addEventListener('click', () => handleCellClick());
+            cell.addEventListener('mouseover', () => handleCellHover(true));
+            cell.addEventListener('mouseout', () => handleCellHover(false));
         });
+    }
+
+    const getSelectorCoordinates = (x, y) => {
+        if (isVertical) return `div.cell[data-x="${x}"][data-y="${y}"]`;
+        else return `div.cell[data-x="${x}"][data-y="${y}"]`;
+    };
+
+    function reserveCells(x, y, length, vertical) {
+        const xInt = parseInt(x);
+        const yInt = parseInt(y);
+        const xLength = xInt + length - 1;
+        const yLength = yInt + length - 1;
+
+        const reserveCell = (selector) => {
+            const cell = map.querySelector(selector);
+            if (cell) cell.classList.add('miss', 'no-click');
+        };
+
+        if (vertical) {
+            for (let index = yInt; index <= yLength; index++) {
+                const selector = getSelectorCoordinates(xInt, index);
+                occupiedCells.push([xInt, index]);
+                reserveCell(selector);
+            }
+        } else {
+            for (let index = xInt; index <= xLength; index++) {
+                const selector = getSelectorCoordinates(index, yInt);
+                occupiedCells.push([index, yInt]);
+                reserveCell(selector);
+            }
+        }
+        console.log(occupiedCells);
+    }
+
+    function getShipCells(x, y, length, vertical) {
+        occupiedByShip = [];
+        const grid = 10;
+        const xInt = parseInt(x);
+        const yInt = parseInt(y);
+        const xLength = xInt + length - 1;
+        const yLength = yInt + length - 1;
+        const xLimit = Math.min(grid, xLength);
+        const yLimit = Math.min(grid, yLength);
+        for (let i = 0; i < length; i++) {
+            const row = vertical ? xInt : xInt + i;
+            const col = vertical ? yInt + i : yInt;
+            if (row <= xLimit && col <= yLimit) {
+                occupiedByShip.push([row, col]);
+            }
+        }
+        return occupiedByShip;
+    }
+
+    function hasOverlap(ship) {
+        let isOverlap = false;
+        for (let i = 0; i < ship.length; i++) {
+            const [x, y] = ship[i];
+            isOverlap = occupiedCells.some((cell) => cell[0] === x && cell[1] === y);
+            if (isOverlap) return true;
+        }
+        return isOverlap;
     }
 
     function isAllowed(x, y, length, vertical) {
         const xInt = parseInt(x) + length - 1;
         const yInt = parseInt(y) + length - 1;
-        let isAllowed = false;
-        if (vertical) isAllowed = grid >= yInt ? true : false;
-        else isAllowed = grid >= xInt ? true : false;
-        return isAllowed;
-    }
-
-    function reserveCells(x, y, length, vertical) {
-        let xInt = parseInt(x);
-        let yInt = parseInt(y);
-        let xLength = xInt + length - 1;
-        let yLength = yInt + length - 1;
-        let cell = '';
-        if (vertical) {
-            for (let index = y - 1; index < yLength; index++) {
-                console.log(index + ': ' + xInt + ' - ' + length);
-                const selector = 'div.cell[data-x="' + xInt + '"][data-y="' + (index + 1) + '"]';
-                cell = map.querySelector(selector);
-                cell.classList.add('miss', 'no-click');
-            }
-        } else {
-            for (let index = x - 1; index < xLength; index++) {
-                console.log(index + ': ' + xInt + ' - ' + length);
-                const selector = 'div.cell[data-x="' + (index + 1) + '"][data-y="' + yInt + '"]';
-                cell = map.querySelector(selector);
-                cell.classList.add('miss', 'no-click');
-            }
-        }
+        const limit = vertical ? yInt : xInt;
+        console.log();
+        return limit <= grid;
     }
 
     function highlightInvalid(x, y, vertical, over) {
-        let xInt = parseInt(x);
-        let yInt = parseInt(y);
-        let cell = '';
-        if (vertical) {
-            for (let index = y - 1; index < grid; index++) {
-                const selector = 'div.cell[data-x="' + xInt + '"][data-y="' + (index + 1) + '"]';
-                cell = map.querySelector(selector);
+        const xInt = parseInt(x);
+        const yInt = parseInt(y);
+
+        const toggleCellHitClass = (selector, over) => {
+            const cell = map.querySelector(selector);
+            if (cell) {
                 if (over) cell.classList.add('hit');
                 else cell.classList.remove('hit');
             }
+        };
+
+        if (vertical) {
+            for (let index = yInt; index <= grid; index++) {
+                const selector = getSelectorCoordinates(xInt, index);
+                toggleCellHitClass(selector, over);
+            }
         } else {
-            for (let index = x - 1; index < grid; index++) {
-                const selector = 'div.cell[data-x="' + (index + 1) + '"][data-y="' + yInt + '"]';
-                cell = map.querySelector(selector);
-                if (over) cell.classList.add('hit');
-                else cell.classList.remove('hit');
+            for (let index = xInt; index <= grid; index++) {
+                const selector = getSelectorCoordinates(index, yInt);
+                toggleCellHitClass(selector, over);
             }
         }
     }
 
-    function hoverNucleusHandler(nucleus, type, over, rotated, permanent) {
+    function hoverNucleus(nucleus, type, over, rotated, permanent) {
         if (over) {
             nucleus.classList.add(type);
             if (rotated) nucleus.classList.add('vertical');
@@ -175,14 +222,15 @@ export const SetupGame = (map, ships) => {
         }
     }
 
-    function clickCellHandler(cell) {
-        selectedShip.x = cell.getAttribute('data-x');
-        selectedShip.y = cell.getAttribute('data-y');
+    function setShipInPlace(cell) {
+        const { x, y } = cell.dataset;
+        selectedShip = { ...selectedShip, x, y };
         cell.classList.add('no-click');
+
         shipsInPlace++;
-        if (shipsInPlace == ships.length) gameReady();
+
+        if (shipsInPlace === ships.length) gameReady();
         else deleteFromList(shipIndex);
-        // events.emit('cell-XY', selectedShip);
     }
 
     function gameReady() {
